0d0feaf94793b80c1f5517ff1e96dc9aa696d8a4
bbasa1 <bbasa@phare.normalesup.org>
2024-05-06 16:09:09 +0200

    Re-menage

--- 00_Main_A_LANCER.R
+++ 00_Main_A_LANCER.R
@@ -121,6 +121,9 @@ print(xtable(dt_recap1[,..l]), include.rownames=FALSE)
 
 
 # Régressions variable politique
+marge_a_50_pct <- 10 # En pourcentage d'écart
+data_loc <- Variable_elections_legislative(copy(filo_merged), marge_a_50_pct)
+
 data_loc[, MAJO_plus_50 := pvoixMAJO >= 0.5]
 dt_recap2 <- Faire_regression_IV(data_loc[Z_instru == 1],var_instru = "MAJO_plus_50",liste_var_reg_12_20, liste_var_reg_13_20, Ponderer_regression,
                                  liste_var_demographie, modeliser_relatif = modeliser_relatif, var_clustering = "LIBCOM", var_controle = "pvoixMAJO")
